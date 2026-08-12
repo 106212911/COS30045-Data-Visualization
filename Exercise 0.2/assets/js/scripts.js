d53e651e467857a925343aed33003158ec7af9c4
Dayaanan <106212911@student.swin.edu.au>
2026-08-12 21:20:38 +0800

    Modifications
    Added closing semantic tags, added new line in js script, added line for toggling of the faq answer to work in index.html final line before semantic closing tags

--- Exercise 0.2/assets/js/scripts.js
+++ Exercise 0.2/assets/js/scripts.js
@@ -3,7 +3,7 @@ console.log(questions); // this checks if its correct or not
 
 questions.forEach(function(question) { // a for each loop 
     question.addEventListener('click', function() { // waits for the click 
-        console.log('clicked'); // opens the answer for the question that has been triggered by a click
+        question.nextElementSibling.classList.toggle('show-answer'); // grabs the answer when I click, and toggle on of the answer if clicked again
     });
 });
 
